0b5521ed0755e1a677c4c21dcb5c6e2bcfb4d47b
sayman <mdrabbi.asm@gmail.com>
2022-04-29 16:20:48 +0600

    process env file add

--- src/Components/firebase.init.js
+++ src/Components/firebase.init.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyBZRN2HpmHO5Plb-KkJygB_HJT9tKCmazc",
-  authDomain: "assignment-11-c3aa0.firebaseapp.com",
-  projectId: "assignment-11-c3aa0",
-  storageBucket: "assignment-11-c3aa0.appspot.com",
-  messagingSenderId: "237879079181",
-  appId: "1:237879079181:web:518f80d3824bfc1b278044"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
