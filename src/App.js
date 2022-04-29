d848c807c57922450677c3aba0c188c8813fb86a
sayman <mdrabbi.asm@gmail.com>
2022-04-29 17:40:35 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div >
-      <h2 className='text-4xl font-bold text-center'>Hello World</h2>
+      <Header></Header>
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
