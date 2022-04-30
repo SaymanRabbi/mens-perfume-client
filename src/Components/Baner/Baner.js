6e105c30929bfa8fce22ce0b5f74aba18e87f61c
sayman <mdrabbi.asm@gmail.com>
2022-04-30 23:09:24 +0600

    baner img set

--- src/Components/Baner/Baner.js
+++ src/Components/Baner/Baner.js
@@ -11,7 +11,7 @@ import './Baner.css'
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 import banerImg from '../../Images/cover_1440_1200.jpeg'
-import banerImg2 from '../../Images/Versace-Flame-Banner.jpeg'
+import banerImg2 from '../../Images/baner2.jpg'
 import banerImg3 from '../../Images/TOP_BANNER_LP_THE_SCENT_PARFUM_blackbackground3.png'
 const Baner = () => {
     return (
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Baner from '../Baner/Baner';
 import Products from '../Products/Products';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
+        <div className='minheight'>
             <Baner></Baner>
              <Products></Products>
         </div>
